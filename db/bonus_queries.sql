0e0411aca1c3995dec3a8c5b12c974db31278127
Dayana Montgomery <dayanamontgomery@Dayanas-MacBook-Air.local>
2023-08-23 01:10:09 -0400

    created the database and tables, user prompts and extra folder for bonus queries

---
+++ db/bonus_queries.sql
@@ -0,0 +1,15 @@
+-- This file addressed the bonus of Challenge 12
+
+-- Query for 'Updating Employee Managers'
+
+-- Query for 'Viewing employees by Manager'
+
+-- Query for 'Viewing Employees by Department'
+
+-- Query for 'Deleting 'Departments'
+
+-- Query for 'Deleting 'Roles'
+
+-- Query for 'Deleting 'Employees'
+
+-- Query for 'Total Employee Salaries by Department'
--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,31 @@
--- Create databases and tables
+-- Application Databases and Tables
+
+-- Creating the database
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+-- Using the employee database
+USE employee_db;
+
+-- Creating a 'Department' table in the Employee Database
+CREATE TABLE department (
+    id INT NOT NULL PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+),
+
+-- Creating a 'Role' table in the Employee Database
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY
+    role_title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT NOT NULL FOREIGN KEY
+),
+
+-- Creating an 'Employee Information' table in the Employee Database
+CREATE TABLE employee_information (
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL FOREIGN KEY, 
+    manager_id VARCHAR(30) FOREIGN KEY
+)
