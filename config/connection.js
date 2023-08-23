e72a7e7a8260813b4f78ca61798c23713f551ffc
Dayana Montgomery <dayanamontgomery@Dayanas-MacBook-Air.local>
2023-08-23 19:43:46 -0400

    added the Sequelize and dotenv npm packages to privatize my credentials

---
+++ config/connection.js
@@ -0,0 +1,17 @@
+// Connecting to Sequelize and requiring Dotenv
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+{
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+}
+);
+
+
+module.exports = sequelize;
